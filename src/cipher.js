59d20a54c3e0f4e778a0ec6f70cc2bf887480d8a
Sharida <60660188+shari12@users.noreply.github.com>
2020-02-14 10:45:53 -0600

    se agrego funcion encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
-const cipher = {
-  // ...
-};
+const cipher ={encode, decode};
+
+function encode(text, offset){
+  let newText ="";
+//recorrer palabra
+  for(let i=0; i < text.length; i++){
+    let numberAscii = text.charCodeAt(i);//obter posicion ascii
+    let formEncode;
+    //posiones donde estan las letras
+
+    formEncode = (( (numberAscii - 65) + parseInt(offset)) % 26) + 65;  //formula para nueva posicion
+    newText += String.fromCharCode(formEncode); //nueva posicion ascii
+
+  }
+  return newText;
+
+}
+
+function decode(text, offset){
+
+
+
+
+}
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import cipher from './cipher.js';
 
+const btnEncode = document.getElementById('btnEncode');
+const btnDecode= document.getElementById('btnDecode');
+
+
+//accionar boton Codificar
+btnEncode.addEventListener('click', function(){
+
+  let textOriginal =document.getElementById('mensaje').value; //obtener valores
+  let numberDesplace = document.getElementById('desplazo').value;
+  let res =  cipher.encode(textOriginal, numberDesplace);
+   document.getElementById('resultMsg').innerHTML= "tu mensaje fue codicado: " + res
+
+ });
+
+
+//accionar boton Decodificar
+btnDecode.addEventListener('click', function(){
+  let textOriginal =document.getElementById('mensaje').value; //obtener valores
+  let numberDesplace = document.getElementById('desplazo').value;
+
+  document.getElementById('resultMsg').innerHTML="tu mensaje ha sido decodificado: "
+});
+
+
 console.log(cipher);
