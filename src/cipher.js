8b1b0cf9348519e43eec8016ed60356c7a1d0a55
Sharida <60660188+shari12@users.noreply.github.com>
2020-02-14 13:19:39 -0600

    funciona decodificar, a partir de la A ya no decodifica

--- src/cipher.js
+++ src/cipher.js
@@ -8,7 +8,6 @@ let formDecodeMayus;
 
 function encode(text, offset){
 
-
   for(let i=0; i < text.length; i++){//empieza el recorrido de la palabra
     numberAscii = text.charCodeAt(i);//obtenemos el ascii de cada letra
     formEncodeMayus = (numberAscii - 65 + parseInt(offset)) % 26 + 65; //formula para nueva posicion mayusculas
@@ -18,18 +17,24 @@ function encode(text, offset){
     }else if (numberAscii >96 && numberAscii<=122) {
       newText += String.fromCharCode(formEncodeMin);
     }
-
   }//termina recorrido
-
   return newText; //regresa el string codificado
-
-
 }//termina funcion encode
 
 function decode(text, offset){
 
+  for(let i =0; i<text.length; i++){
+    numberAscii = text.charCodeAt(i);
+    formDecodeMayus = (numberAscii - 65 - parseInt(offset)) % 26 + 65;
+    formDecodeMin = (numberAscii - 97 - parseInt(offset)) % 26 + 97;
 
-
+    if( numberAscii >64 && numberAscii<=90){
+      newText += String.fromCharCode(formDecodeMayus);
+    }else if (numberAscii >96 && numberAscii<=122) {
+      newText += String.fromCharCode(formDecodeMin);
+    }
+  }
+  return newText;
 
 }
 
