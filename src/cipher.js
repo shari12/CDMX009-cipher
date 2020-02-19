1846a24825cf0b5a3aa5914dde192a8992404ea5
Sharida <60660188+shari12@users.noreply.github.com>
2020-02-18 20:21:25 -0600

    Se agrego boton de limpiar

--- src/cipher.js
+++ src/cipher.js
@@ -41,3 +41,31 @@ function decode(offset,text){
 
 
 export default cipher;
+
+
+// function cipherOperation(offset, text){
+//   let newText = "";
+//   let numberAscii;
+//   let formEncodeMayus;
+//   let formDecodeMayus;
+//
+//   if (typeof text === "string"){
+//     for (let i = 0; i < text.length; i++){
+//       numberAscii = text.charAt(i);
+//       switch (cipherOperation) {
+//         case encode:
+//         formEncodeMayus = (numberAscii - 65 + parseInt(offset)) % 26 + 65; //formula para nueva posicion mayusculas
+//          newText += String.fromCharCode(formEncodeMayus); //pasar a string la nueva posicion ascii
+//           break;
+//         case decode:
+//         formDecodeMayus = (numberAscii + 65 - parseInt(offset)) % 26 + 65;
+//         newText += String.fromCharCode(formDecodeMayus);
+//         break;
+//       }
+//     }// termina recorrido
+//     return newText;
+//
+//   }else{
+//     throw TypeError("ingresaste un caracter invalido ")
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -1,46 +1,50 @@
 import cipher from './cipher.js';
 //obtener botones y agregar funciones al hace click
-document.getElementById('btnEncode').addEventListener('click',getValuesEncode);
-  document.getElementById('btnDecode').addEventListener('click', getValuesDecode);
+document.getElementById('btnEncode').addEventListener('click', getValuesEncode);
+document.getElementById('btnDecode').addEventListener('click', getValuesDecode);
+document.getElementById('btnCleaner').addEventListener('click', clean);
 
-function getValuesEncode(){//obtenemos los valores de index.html
+function getValuesEncode() { //obtenemos los valores de index.html
   let textOriginal = document.getElementById('mensaje').value;
   let numberDesplace = document.getElementById('desplazo').value;
-  let encoderes = cipher.encode(numberDesplace,textOriginal);//asignamos una variable y traemos la funcion de encode
+  let encoderes = cipher.encode(numberDesplace, textOriginal); //asignamos una variable y traemos la funcion de encode
 
-  if(textOriginal==textOriginal.toLowerCase()){//verificamos si estan en minisculas
+  if (textOriginal == textOriginal.toLowerCase()) { //verificamos si estan en minisculas
     alert("por favor ingresa tu mensaje en mayusculas ");
-  }else if (numberDesplace==="") {//verifica que la caja de desplazo no esta vacia
+  } else if (numberDesplace === "") { //verifica que la caja de desplazo no esta vacia
     alert("por favor asegurate de tener un numero de desplazo ");
-  }
-  else{
-   document.getElementById('resultMsg').innerHTML="Tu mensaje fue codificado: " + encoderes;// mostramos el resultado en index.html
+  } else {
+    document.getElementById('resultMsg').innerHTML = "Tu mensaje fue codificado: " + encoderes; // mostramos el resultado en index.html
 
   }
 
 
 }
 
-function getValuesDecode(){
+function getValuesDecode() {
   let textOriginal = document.getElementById('mensaje').value;
   let numberDesplace = document.getElementById('desplazo').value;
-  let decoderes = cipher.decode(numberDesplace,textOriginal);
+  let decoderes = cipher.decode(numberDesplace, textOriginal);
 
-  if(textOriginal==textOriginal.toLowerCase()){
+  if (textOriginal == textOriginal.toLowerCase()) {
     alert("por favor ingresa tu mensaje en mayusculas ");
-  }else if (numberDesplace==="") {
+  } else if (numberDesplace === "") {
     alert("por favor asegurate de tener un numero de desplazo ");
 
-  }
+  } else {
 
-  else{
-
-    document.getElementById('resultMsg').innerHTML="Tu mensaje fue codificado: " + decoderes;
+    document.getElementById('resultMsg').innerHTML = "Tu mensaje fue codificado: " + decoderes;
   }
 
 }
 
+function clean(){
+  //location.reload();
+  document.getElementById('mensaje').value="";
+  document.getElementById('desplazo').value="";
+  document.getElementById('resultMsg').innerHTML="";
 
+}
 
 
 
